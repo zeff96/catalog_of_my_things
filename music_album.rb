f21ac7ef69f600b300b0a9b1012afb0571ecaf49
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-05-29 18:36:23 +0300

    fix typo error

--- music_album.rb
+++ music_album.rb
@@ -3,12 +3,15 @@ require_relative 'item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(publish_date, on_spotify)
+  def initialize(publish_date, on_spotify : false)
     super(publish_date)
     @on_spotify = on_spotify
   end
+
+  private
 
   def can_be_archived?
     super && on_spotify
   end
 end
+
