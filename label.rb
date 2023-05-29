f10091681a166096e3e030cd0c1a7167cf9c4601
c00p75 <georgecoopmsapenda@gmail.com>
2023-05-29 17:02:37 +0200

    fix linter errors

--- label.rb
+++ label.rb
@@ -1,6 +1,7 @@
 class Label
   attr_accessor :title, :color
-  attr_reader :id, :items 
+  attr_reader :id, :items
+
   def initialize(title, color)
     @id = Random.rand(1..1000)
     @title = title
