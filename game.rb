90e66df5538d80946192819f7e32cc5363fdadf5
zeff96 <zeffo1975@gmail.com>
2023-05-29 15:08:26 +0300

    Add private method to validate played at > 2 years.

--- game.rb
+++ game.rb
@@ -7,4 +7,13 @@ class Game
   def can_be_archived?
     super && played_last_at?
   end
+
+  private
+
+  def played_last_at?
+    current_year = Time.now.year
+    last_played_year = @last_palyed_at.year
+    archived = current_year - last_played_year
+    archived > 2
+  end
 end
