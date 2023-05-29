6ed0b92599d268ab187b3c0bc688d93395234248
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-05-29 17:31:57 +0300

    initialize method

--- music_album.rb
+++ music_album.rb
@@ -1,4 +1,11 @@
 require_relative 'item'
 
 class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(publish_date, on_spotify)
+    super(publish_date)
+    @on_spotify = on_spotif
+  end
+
 end
