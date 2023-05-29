9cd739af9bd975012ad004a118d1f0269d69f2ca
zeff96 <zeffo1975@gmail.com>
2023-05-29 15:19:06 +0300

    Add generate id private method.

--- item.rb
+++ item.rb
@@ -1,4 +1,7 @@
-class Item
+class 
+  attr_reader :id
+  attr_accessor :publish_date
+  
   def initialize(publish_date)
     id = generate_id
     genre = nil
@@ -7,5 +10,11 @@ class Item
     label = nil
     @publish_date = publish_date
     archived = false
+  end
+
+  private
+
+  def generate_id
+    Random.rand(1..1000)
   end
 end
