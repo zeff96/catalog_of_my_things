fdc777eca0cf7398e1ac09575aa62d19458dc768
zeff96 <zeffo1975@gmail.com>
2023-05-30 16:42:02 +0300

    Add class instance validation.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,4 +4,14 @@ describe Game do
   before(:each) do
     @game = Game.new('Yes', '2005-10-15')
   end
+
+  context 'Game class test' do
+    it 'should be an instance of Game class' do
+      expect(@game).to be_an_instance_of Game
+    end
+
+    it 'multiplayer should eq "Yes"' do
+      expect(@game.multiplayer).to eq 'Yes'
+    end
+  end
 end
