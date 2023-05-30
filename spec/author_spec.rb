bccadf31a90ff0c12582efb44ddb0cb0ef9b021f
zeff96 <zeffo1975@gmail.com>
2023-05-30 16:29:37 +0300

    Add error scope fix.

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,26 +3,33 @@ require_relative '../lib/game'
 
 
 describe Author do
+  before(:each) do
+    @author = Author.new('zeff', 'adeka')
+  end
+
   context 'Author class test' do
-    author = Author.new('zeff', 'adeka')
 
     it 'should be an instance of author class' do
-      expect(author).to be_an_instance_of(Author)
+      expect(@author).to be_an_instance_of(Author)
     end
 
     it 'should correct first_name' do
-      expect(author.first_name).to eq 'zeff'
+      expect(@author.first_name).to eq 'zeff'
     end
 
     it 'should correct last_name' do
-      expect(author.last_name).to eq 'adeka'
+      expect(@author.last_name).to eq 'adeka'
     end
 
-    item = Game.new('Yes', '2022-10-15')
-    author.add_items(item)
-
+    it 'should have empty list items' do
+      expect(@author.items).to eq []
+    end
+    
     it 'should have an item after calling add_items' do
-      expect(author.items.length).to eq 1
+      item = Game.new('Yes', '2022-10-15')
+      @author.add_items(item)
+
+      expect(@author.items.length).to eq 1
     end
   end
 end
