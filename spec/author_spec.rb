a2cb0e987ddd728edcd29d438d1da9d602ae4747
zeff96 <zeffo1975@gmail.com>
2023-05-30 15:26:49 +0300

    Add unit test to validate class instantiation.

---
+++ spec/author_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/author'
+require_relative '../lib/game'
+
+
+describe Author do
+  context 'Author class test' do
+    author = Author.new('zeff', 'adeka')
+
+    it 'should be an instance of author class' do
+      expect(author).to be_an_instance_of(Author)
+    end
+
+    it 'should correct first_name' do
+      
+    end
+  end
+end
