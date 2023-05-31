f4bcbed21015a9d4c6d68acd43d60b8bb542429c
zeff96 <zeffo1975@gmail.com>
2023-05-31 16:28:20 +0300

    Add file path for saveItems.

--- lib/app.rb
+++ lib/app.rb
@@ -2,11 +2,13 @@ require_relative '../modules/add_author'
 require_relative '../modules/list_items'
 require_relative '../modules/add_game'
 require_relative '../modules/json'
+require_relative '../modules/save_data'
 
 class App
   include CreateAuthor
   include Listitems
   include CreateGame
+  include SaveItems
 
   def initialize
     @save_author_data = SaveData.new('../data/author.json')
