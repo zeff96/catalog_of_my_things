f89335c7808a2e3dd6cf5a6603c0356dd542154a
zeff96 <zeffo1975@gmail.com>
2023-05-31 16:52:27 +0300

    Add method for saving game data to json.

--- modules/save_data.rb
+++ modules/save_data.rb
@@ -2,4 +2,8 @@ module SaveItems
   def save_author_data
     @save_author_data.save_data(@authors.map(&:to_hash))
   end
+
+  def save_game_data
+    @save_game_data.save_data(@games.map(&:to_hash))
+  end
 end
