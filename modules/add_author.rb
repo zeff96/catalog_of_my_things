423bc99ac3824ea119661fabcef22267c7ac2d8d
zeff96 <zeffo1975@gmail.com>
2023-05-31 10:41:14 +0300

    Add list_all_authors method for listing all authors.

--- modules/add_author.rb
+++ modules/add_author.rb
@@ -8,4 +8,13 @@ module AuthorManager
     author = Author.new(first_name, last_name)
     @authors << author unless author.nil?
   end
+
+  def list_all_authors
+    puts "List of all authors"
+    print "No author found\n" if @authors.empty?
+
+    @authors.each_with_index do |author, idx|
+      puts "#{idx + 1}.#{author.first_name} #{author.last_name}"
+    end
+  end
 end
