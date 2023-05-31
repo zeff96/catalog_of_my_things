7c8e4930b1c0a8a968ba356d1c674e1ff6fd93d1
zeff96 <zeffo1975@gmail.com>
2023-05-31 16:12:31 +0300

    Add save_data method for saving data.

--- modules/save_data.rb
+++ modules/save_data.rb
@@ -4,4 +4,9 @@ class SaveData
   def initialize(file_name)
     @file_name = file_name
   end
+
+  def save_data(data)
+    File.write(@file_name, JSON.pretty_generate(data))
+    puts 'Data saved successfully!'
+  end
 end
