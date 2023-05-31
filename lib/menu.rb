9478d7d36ed611028ea251e99ac8233044a291cc
zeff96 <zeffo1975@gmail.com>
2023-05-31 10:43:46 +0300

    Add menu option lists.

---
+++ lib/menu.rb
@@ -0,0 +1,5 @@
+OPTIONS = {
+  1 => {label: 'List all authors', action: :list_all_authors},
+  2 => {label: 'Add author', action: :add_author},
+  3 => {label: 'Exit', action: :exit_program}
+}.freeze
