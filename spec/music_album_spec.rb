13545c14151e7b42014591b4d680f1b9a60333e6
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-06-01 14:06:11 +0300

    Add unit test to hash method in music album

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -2,37 +2,38 @@ require_relative '../lib/music_album'
 require 'date'
 require_relative '../lib/item'
 
-describe MusicAlbum do
-  before(:each) do
-    @album = MusicAlbum.new('2010-01-01', true)
-  end
 
-  context 'music album class test' do
-    it 'be an instance of MusicAlbum' do
-      expect(@album).to be_an_instance_of MusicAlbum
-    end
+  describe MusicAlbum do
+  let(:publish_date) { '2022-01-01' }
+  let(:on_spotify) { true }
+  let(:music_album) { MusicAlbum.new(publish_date, on_spotify) }
 
-    it 'inherit from Item class' do
-      expect(@album).to be_a(Item)
-    end
-
-    it 'move_to_archive to eq "true"' do
-      expect(@album.move_to_archive).to be_truthy
-    end
-
-    it 'should return true if publish date is older than 10yrs and on Spotify' do
-      expect(@album.send(:can_be_archived?)).to be_truthy
-    end
-
-    it 'should return false if publish date is older than 10yrs but not on Spotify' do
-      album = MusicAlbum.new('2010-01-01', false)
-      expect(album.send(:can_be_archived?)).to be_falsy
+  describe '#initialize' do
+    it 'sets the publish_date and on_spotify attributes' do
+      expect(music_album.publish_date).to eq(publish_date)
+      expect(music_album.on_spotify).to eq(on_spotify)
     end
   end
 
-  context 'item class test' do
-    it 'should return true if publish date is older than 10yrs' do
-      expect(@album.send(:can_be_archived?)).to be_truthy
+  describe '#to_hash' do
+    it 'returns a hash with the publish_date and on_spotify' do
+      expected_hash = {
+        publish_date: publish_date,
+        on_spotify: on_spotify
+      }
+      expect(music_album.to_hash).to eq(expected_hash)
+    end
+  end
+
+  describe '#can_be_archived?' do
+    it 'returns true if the item can be archived and on_spotify is true' do
+      allow(Date).to receive(:today).and_return(Date.new(2033, 1, 1))
+      expect(music_album.send(:can_be_archived?)).to be_truthy
+    end
+
+    it 'returns false if the item cannot be archived' do
+      allow(Date).to receive(:today).and_return(Date.new(2030, 1, 1))
+      expect(music_album.send(:can_be_archived?)).to be_falsey
     end
   end
 end
